be38bbb577e50c078a9f4033c68e18b40a668a43
matthaeuss <91155894+matthaeuss@users.noreply.github.com>
2022-04-13 22:03:57 +0200

    callback hell

--- src/lib/callback.ts
+++ src/lib/callback.ts
@@ -1,8 +1,31 @@
-import { Callback } from "./interfaces";
+import { Callback, CallbackError, Hero } from "./interfaces";
+import axios, { AxiosError } from "axios"
+import {apiUrl} from "./config"
+import { AxiosResponse } from "axios";
+import {parseList} from "./config"
+
+
+function getHeroCallback(
+    email: string, 
+    callback: Callback<Hero>,
+    callbackError?: CallbackError
+) {
+    axios
+        .get<Hero[]>(`${apiUrl}/heroes/email=${email}`)
+        .then((response: AxiosResponse<Hero[]>) => {
+            const data = parseList<Hero>(response);
+            const hero = data[0];
+            callback(hero);
+        })
+        .catch((error: AxiosError) => {
+            callbackError('We are unable to fetch Hero');
+        });
+}
 
 const getHeroTreeCallback = function(
     email: string,
-    callback: Callback<Hero>
-){
-
-}
+    callback: Callback<Hero>,
+    callbackError?: CallbackError,
+) {
+    getHeroCallback(email, () => {}, (error: string) => {callbackError(error)});
+};
